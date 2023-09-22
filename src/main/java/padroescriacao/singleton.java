07fbf3171dacc6619fcf29bd70e36a29f77d6e82
Carlos <carloskinoko@gmail.com>
2023-09-22 15:16:21 -0300

    name changes and diagram

--- src/main/java/padroescriacao/singleton.java
+++ src/main/java/padroescriacao/singleton.java
@@ -1,2 +1,25 @@
-package padroescriacao;public class singleton {
+package padroescriacao;
+
+public class DadosMoeda {
+
+    private DadosMoeda() {};
+
+    private static DadosMoeda instance = new DadosMoeda();
+    public static DadosMoeda getInstance() {return instance;}
+
+    private double valorMoeda;
+
+    private String nomeMoeda;
+
+    public String getNomeMoeda() {return nomeMoeda;}
+
+    public void setNomeMoeda(String nomeMoeda) {this.nomeMoeda = nomeMoeda;}
+
+    public double getValorMoeda() {return this.valorMoeda;}
+
+    public void setValorMoeda(double valorMoeda) {this.valorMoeda = valorMoeda;}
+
+
+
+
 }
--- src/test/java/padroescriacao/ParametrosTest.java
+++ src/test/java/padroescriacao/ParametrosTest.java
@@ -1,2 +1,18 @@
-package padroescriacao;public class ParametrosTest {
+package padroescriacao;
+
+
+class dadosMoedaTest {
+
+    @Test
+    public void deveRetornarValorMoeda() {
+        DadosMoeda.getInstance().setValorMoeda(8.11);
+        assertEquals(4.1, DadosMoeda.getInstance().getValorMoeda());
+    }
+
+    @Test
+    public void deveretornarNomeMoeda() {
+        DadosMoeda.getInstance().setNomeMoeda("dolar");
+        assertEquals("dolar", DadosMoeda.getInstance().getNomeMoeda());
+    }
+
 }
